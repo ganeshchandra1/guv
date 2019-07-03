fe70b343746011bdc1a3c83f6b5b40722af4b9fa
ganeshchandra1 <48551837+ganeshchandra1@users.noreply.github.com>
2019-07-03 09:25:14 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,9 @@
 a=str(input())
-if(a=="a" or a=="e" or a=="i" or a=="o" or a=="u"):
-  print("Vowel")
-elif(a=="b" or a=="c" or a=="d" or a="f" or a="g" or a="h" or a="j" or a="k" or a="l" or a="m" or a="n" or a="p" or a="q" or a="r" or a="s" or a="t" or a="v" or a="w" or a="x" or a="y" or a="z):
-     print("Consonant")
+list1=["a","e","i","o","u"]
+list2=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if a in list1:
+    print("Vowel")
+elif a in list2:
+    print("Consonant")
 else:
-     print("Invalid")
+    print("Invalid")
