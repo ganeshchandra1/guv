0baa9d0383a2af9a1b146a49f774c5ad174509e0
ganeshchandra1 <48551837+ganeshchandra1@users.noreply.github.com>
2019-07-03 09:18:12 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,9 +1,7 @@
 a=str(input())
-vowel="aeiou"
-consonant="bcdfghjklmnpqrrstvwxyz"
-for i in range(0,len(vowel))
-  if(a==vowel[i]):
-    print("Vowel")
-  
-
-  print("Consonant")
+if(a=="a" or a=="e" or a=="i" or a=="o" or a=="u"):
+  print("Vowel")
+elif(a=="b" or a=="c" or a=="d" or a="f" or a="g" or a="h" or a="j" or a="k" or a="l" or a="m" or a="n" or a="p" or a="q" or a="r" or a="s" or a="t" or a="v" or a="w" or a="x" or a="y" or a="z):
+     print("Consonant")
+else:
+     print("Invalid")
