be8b378ecbaa5f0322ade7d62eba9e5dd8c3de92
ganeshchandra1 <48551837+ganeshchandra1@users.noreply.github.com>
2019-07-05 21:59:26 +0530

    Update player19.py

--- player19.py
+++ player19.py
@@ -1,12 +1,17 @@
 import math
 n=int(input())
-while(n%2==0):
-    print("2",end=' ')
-    n=n//2
-for i in range(3,int(math.sqrt(n))+1,2):
-    while(n%i==0):
-        print(i)
-        break
-        n=n//i
-    else:
-        print(n)
+a=n
+list1=[]
+while n % 2 == 0: 
+    print(2,end=' ') 
+    n = n / 2
+        
+for i in range(3, int(math.sqrt(n))+1, 2): 
+    
+    while n % i == 0: 
+        if i not in list1:
+             list1.append(i)
+             n=n/i
+             print(','.join(map(str, list1)))
+if n > 2: 
+    print(n) 
