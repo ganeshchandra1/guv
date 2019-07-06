0f6a206f9395b15d3e7f82f23e8cf45f66c5d652
ganeshchandra1 <48551837+ganeshchandra1@users.noreply.github.com>
2019-07-06 21:30:21 +0530

    Create hunter1.py

---
+++ hunter1.py
@@ -0,0 +1,8 @@
+n=int(input())
+list1=[]
+list2=[]
+list1=list(input().split())
+for i in range(0,len(list1)):
+    if (list1.count(list1[i])>=2):
+        list2.append(list1[i])
+print(' '.join(sorted(set(list2))))
