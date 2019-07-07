4ea8cc24365ea300d05083a11ea414a96a1fde53
ganeshchandra1 <48551837+ganeshchandra1@users.noreply.github.com>
2019-07-07 20:56:30 +0530

    Update hunter1.py

--- hunter1.py
+++ hunter1.py
@@ -5,4 +5,7 @@ list1=list(input().split())
 for i in range(0,len(list1)):
     if (list1.count(list1[i])>=2):
         list2.append(list1[i])
-print(' '.join(sorted(set(list2))))
+if(len(list2)==0):
+    print("unique")
+else:
+    print(' '.join(sorted(set(list2))))
