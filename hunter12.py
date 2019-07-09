e157da198707a1e31b9d5f7bc6def02d089a1bde
ganeshchandra1 <48551837+ganeshchandra1@users.noreply.github.com>
2019-07-09 22:15:43 +0530

    Update hunter12.py

--- hunter12.py
+++ hunter12.py
@@ -1,3 +1,8 @@
-n,k=map(int,input().split())
-l=sorted(list(input().split()))
-print(l[-k])
+n,m=map(int,input().split())
+k=input().split()
+l2=[]
+l3=[]
+for i in k:
+    l2.append(int(i))
+l3=sorted(l2)
+print(l3[-m])
