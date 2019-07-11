6dcbc96693332461a37d5781d1ab8818cb7d36b3
ganeshchandra1 <48551837+ganeshchandra1@users.noreply.github.com>
2019-07-11 18:05:59 +0530

    Create pro6.py

---
+++ pro6.py
@@ -0,0 +1,13 @@
+n,m=map(int,input().split())
+l=[]
+l2=[]
+for i in range(n):
+        l.append(input())
+for i in l:
+    l2.append(i.count('1'))
+if 0 in l2:
+    l2.remove(0)
+for i in range(min(l2)):
+    for j in range(min(l2)):
+        print("1",end=' ')
+    print(end='\n')
