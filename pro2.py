a2b97902312487e95e7af9f9c13291e37852e75c
ganeshchandra1 <48551837+ganeshchandra1@users.noreply.github.com>
2019-07-11 13:49:03 +0530

    Rename pro1.py to pro2.py

---
+++ pro2.py
@@ -0,0 +1,9 @@
+n,q=map(int,input().split())
+l=list(map(int,input().split()))
+lis=[]
+li=[]
+for i in range(q):
+    lis=input().split()
+    li.append(sum(l[int(lis[0])-1:int(lis[1])]))
+for i in li:
+    print(i,end='\n'
